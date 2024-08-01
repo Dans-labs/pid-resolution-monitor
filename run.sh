0cbcf485f53c02854a8cb6bc92c3fc76ad0baf24
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2024-08-01 13:36:47 +0200

    Add celery default task

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 uvicorn main:app --host 0.0.0.0 --port 9000 &
-celery -A main.celery worker -l INFO -Q pid-resolution,pidmr --autoscale=1,10
+celery -A main.celery worker -l INFO -Q pid-resolution,pidmr,celery --autoscale=1,10
