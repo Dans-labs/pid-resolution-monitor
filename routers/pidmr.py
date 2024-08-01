ee263c29685d33485fa9d1b8c5acfc59452c0486
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2024-08-01 11:26:19 +0200

    Resolve PID after PIDMR event.

--- routers/pidmr.py
+++ routers/pidmr.py
@@ -1,5 +1,3 @@
-from typing import Annotated
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -8,6 +6,9 @@ from database.database import get_db
 from logging_config import pidmr_logger as logger
 from routers.users import get_current_enabled_user
 from schemas.schemas import PIDMResolutionEvent, User
+from celeryworker.tasks import resolve_pid_task
+
+from typing import Annotated
 
 router = APIRouter(
     prefix="/pidmr",
@@ -17,13 +18,14 @@ router = APIRouter(
 
 
 @router.post("/event")
-async def create_event(event: PIDMResolutionEvent, user: Annotated[User, Depends(get_current_enabled_user)],
-                       db: Session = Depends(get_db)):
+async def create_event(event: PIDMResolutionEvent, user: Annotated[User, Depends(get_current_enabled_user)], db: Session = Depends(get_db)):
     try:
         db_event = create_pidmr_event(db=db, event=event)
         if not db_event:
             raise HTTPException(status_code=400, detail="Error saving event")  # 400 to 499 are client error codes.
         logger.info(f"PIDMR event saved: {db_event.pid_endpoint} by user {user.username}")
+        # Add a celery task to resolve this PID:
+        resolve_pid_task.delay(db_event.pid_endpoint)
         return {"event_id": db_event.id, "pid_endpoint": db_event.pid_endpoint}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
